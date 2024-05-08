7cd8d135d99e92ad270cde12aa6b0b39021071b9
Cr-Israel <carlosisrael08@hotmail.com>
2024-05-08 17:28:27 -0300

    first commit

---
+++ streams_node/stream_http_server.js
@@ -0,0 +1,28 @@
+import http from "node:http"
+import { Transform } from "node:stream"
+
+class InverseNumberStream extends Transform {
+  _transform(chunck, encoding, callback) {
+    const transformed = Number(chunck.toString()) * -1
+
+    console.log(transformed)
+
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+const server = http.createServer(async (request, response) => {
+  const buffers = []
+
+  for await (const chunck of request) {
+    buffers.push(chunck)
+  }
+
+  const fullStreamContent = Buffer.concat(buffers).toString()
+
+  console.log(fullStreamContent)
+
+  return response.end(fullStreamContent)
+})
+
+server.listen(3334)
